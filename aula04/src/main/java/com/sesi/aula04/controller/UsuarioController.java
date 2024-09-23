f9176d4cf749560cff0f0b5790710e1ec67e882a
edumoren0 <158178844+edumoren0@users.noreply.github.com>
2024-09-23 11:52:52 -0300

    Add files via upload

---
+++ aula04/src/main/java/com/sesi/aula04/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.sesi.aula04.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sesi.aula04.Repository.UsuarioRepository;
+
+@Controller
+public class UsuarioController {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/usuarios")
+	public String listarUsuario(Model modelo) {
+		modelo.addAttribute("usuarios", usuarioRepository.findAll());
+		
+		return "usuarios";
+	}
+
+	
+	
+}
+
